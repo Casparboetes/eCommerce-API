50013099a25f8e283cf2c1850d18c1153e0bf2ee
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-04-23 12:10:42 +0200

    Typo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3030
 let app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello from expess')
+  res.send('Hello from express')
 })
 
 app.listen(PORT, () => {
